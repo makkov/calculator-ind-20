ac37b57acb267dce4411e90244a08e3d406898b2
makkov <maxim_86nva@mail.ru>
2024-03-16 09:52:24 +0300

    init project

---
+++ src/main/java/ru/skypro/calculatorind20/CalculatorInd20Application.java
@@ -0,0 +1,13 @@
+package ru.skypro.calculatorind20;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorInd20Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorInd20Application.class, args);
+	}
+
+}
---
+++ src/test/java/ru/skypro/calculatorind20/CalculatorInd20ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skypro.calculatorind20;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorInd20ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
